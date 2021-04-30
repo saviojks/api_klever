0da9ad321d4503ade84991e91fb69fef144990ba
saviooribe <saviooribe@gmail.com>
2021-04-30 00:05:10 -0300

    fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const server = require('http').createServer(app)
 const socket_io = require('socket.io')
 const io = socket_io(server)
 const morgan = require('morgan')
+const BlockList = require('./services/Blockbook')
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', "*");
@@ -38,14 +39,12 @@ app.post('/balance/:address', async (request, response) => {
 		let total_confirmed = 0
 		let total_unconfirmed = 0
 
-		const responseBlockBook = await Blockbook.view(address)
+		const responseBlockBook = await BlockList.view(address)
 		if (responseBlockBook.error) {
-			console.log('responseBlockBook', responseBlockBook.error.response)
 			return response.status(responseBlockBook.error.response.status).send({ message: responseBlockBook.error.response.data.error })
 		}
 		const addresses = responseBlockBook.data
 		for (let item of addresses) {
-			console.log('item', item)
 			if (item.confirmations < 2) {
 				total_unconfirmed += parseInt(item.value)
 				request.io.emit('new_unconfirmed', total_unconfirmed)
@@ -58,6 +57,6 @@ app.post('/balance/:address', async (request, response) => {
 
 		return response.status(200).send({ response: responseBlockBook.data, confirmed: total_confirmed, unconfirmed: total_unconfirmed })
 	} catch (err) {
-		return response.status(400).send({ message: "ops, something went wrong!" })
+		return response.status(400).send({ message: "ops, something went wrong!", error: err })
 	}
 })
